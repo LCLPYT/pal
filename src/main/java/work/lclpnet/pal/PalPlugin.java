1bdeb5426ffbebc790a701529f98b8c44dc3c1ac
LCLP <lukas@lclpnet.work>
2023-07-14 19:20:37 +0200

    use kibu-plugin provided TranslationService

--- src/main/java/work/lclpnet/pal/PalPlugin.java
+++ src/main/java/work/lclpnet/pal/PalPlugin.java
@@ -2,38 +2,46 @@ package work.lclpnet.pal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import work.lclpnet.kibu.access.PlayerLanguage;
-import work.lclpnet.kibu.plugin.KibuPlugin;
+import work.lclpnet.kibu.plugin.ext.KibuPlugin;
+import work.lclpnet.kibu.plugin.ext.TranslatedPlugin;
 import work.lclpnet.kibu.plugin.hook.HookListenerModule;
+import work.lclpnet.kibu.plugin.hook.TranslationsLoadedCallback;
 import work.lclpnet.kibu.translate.TranslationService;
 import work.lclpnet.pal.cmd.PalCommand;
 import work.lclpnet.pal.di.DaggerPalComponent;
 import work.lclpnet.pal.di.PalComponent;
 import work.lclpnet.pal.di.PalModule;
-import work.lclpnet.translations.DefaultLanguageTranslator;
 import work.lclpnet.translations.loader.translation.SPITranslationLoader;
+import work.lclpnet.translations.loader.translation.TranslationLoader;
 
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-public class PalPlugin extends KibuPlugin {
+public class PalPlugin extends KibuPlugin implements TranslatedPlugin {
 
     public static final String ID = "pal";
     private static final Logger logger = LoggerFactory.getLogger(ID);
+    private TranslationService translationService = null;
     private PalComponent component = null;
+    private TranslationsLoadedCallback translationHook;
 
     @Override
     public void loadKibuPlugin() {
-        TranslationService translationService = createTranslationService();
-
         component = DaggerPalComponent.builder()
                 .palModule(new PalModule(logger, translationService))
                 .build();
 
-        var translator = (DefaultLanguageTranslator) translationService.getTranslator();
+        final CompletableFuture<Void> translationsFuture = new CompletableFuture<>();
+
+        registerHook(TranslationsLoadedCallback.HOOK, translationHook = service -> {
+            if (translationHook != null && service.getTranslator().hasTranslation("en_us", "pal.permissions.requires.living")) {
+                translationsFuture.complete(null);
+                unregisterHook(TranslationsLoadedCallback.HOOK, translationHook);
+                translationHook = null;
+            }
+        });
 
         CompletableFuture.allOf(
-                translator.reload(),
+                translationsFuture,
                 component.configManager().init()
         ).exceptionally(err -> {
             // handle any errors and recover
@@ -52,11 +60,14 @@ public class PalPlugin extends KibuPlugin {
         }
     }
 
-    private TranslationService createTranslationService() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        SPITranslationLoader loader = new SPITranslationLoader(classLoader);
-        DefaultLanguageTranslator translator = new DefaultLanguageTranslator(loader);
+    @Override
+    public void injectTranslationService(TranslationService translationService) {
+        this.translationService = translationService;
+    }
 
-        return new TranslationService(translator, player -> Optional.of(PlayerLanguage.getLanguage(player)));
+    @Override
+    public TranslationLoader createTranslationLoader() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new SPITranslationLoader(classLoader);
     }
 }
