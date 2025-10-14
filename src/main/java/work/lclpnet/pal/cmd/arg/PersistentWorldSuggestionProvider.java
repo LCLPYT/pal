a776b7230e660a54831079319a006e72cd928085
LCLP <lukas@lclpnet.work>
2025-10-14 20:04:47 +0200

    vastly improve performance of available world suggestions

--- src/main/java/work/lclpnet/pal/cmd/arg/PersistentWorldSuggestionProvider.java
+++ src/main/java/work/lclpnet/pal/cmd/arg/PersistentWorldSuggestionProvider.java
@@ -9,12 +9,14 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
 import net.minecraft.world.level.storage.LevelStorage;
+import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
 import work.lclpnet.kibu.world.mixin.MinecraftServerAccessor;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Iterator;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 import java.util.concurrent.CompletableFuture;
 
 public class PersistentWorldSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
@@ -27,31 +29,37 @@ public class PersistentWorldSuggestionProvider implements SuggestionProvider<Ser
         Path dimDirectory = session.getDirectory(WorldSavePath.ROOT).resolve("dimensions");
 
         return CompletableFuture.supplyAsync(() -> {
-            try (var files = Files.find(dimDirectory, 16, (path, attr)
-                    -> dimDirectory.relativize(path).getNameCount() >= 2)) {
+            try {
+                Files.walkFileTree(dimDirectory, EnumSet.noneOf(FileVisitOption.class), 16, new SimpleFileVisitor<>() {
 
-                files.filter(path -> Files.isRegularFile(path.resolve("level.dat")))
-                        .map(path -> {
-                            Path rel = dimDirectory.relativize(path);
+                    @Override
+                    public @NotNull FileVisitResult preVisitDirectory(@NotNull Path dir, @NotNull BasicFileAttributes attrs) {
+                        if (!Files.isRegularFile(dir.resolve("level.dat"))) {
+                            return FileVisitResult.CONTINUE;
+                        }
 
-                            var it = rel.iterator();
-                            String namespace = it.next().toString();
-                            StringBuilder pathBuilder = new StringBuilder();
+                        Path rel = dimDirectory.relativize(dir);
 
-                            while (it.hasNext()) {
-                                if (!pathBuilder.isEmpty()) {
-                                    pathBuilder.append('/');
-                                }
+                        if (rel.getNameCount() < 2) {
+                            return FileVisitResult.CONTINUE;
+                        }
 
-                                pathBuilder.append(it.next());
-                            }
+                        var it = rel.iterator();
+                        String namespace = it.next().toString();
+                        StringBuilder pathBuilder = new StringBuilder();
 
-                            Identifier id = Identifier.of(namespace, pathBuilder.toString());
+                        while (it.hasNext()) {
+                            if (!pathBuilder.isEmpty()) pathBuilder.append('/');
 
-                            return id.toString();
-                        })
-                        .forEach(builder::suggest);
+                            pathBuilder.append(it.next());
+                        }
 
+                        Identifier id = Identifier.of(namespace, pathBuilder.toString());
+                        builder.suggest(id.toString());
+
+                        return FileVisitResult.SKIP_SUBTREE;
+                    }
+                });
             } catch (IOException ignored) {}
 
             return builder.build();
